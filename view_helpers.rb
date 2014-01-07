bcce4a83f63d63843cd5029366732bf306b618d0
Casper Lai <casper.lai@sleepingdesign.com>
2014-01-07 15:08:05 +0800

    add view helpers. It’s help to create link and if request path is current page, it’s automatic add the ‘active’ class.

---
+++ view_helpers.rb
@@ -0,0 +1,32 @@
+# This method is base on fireapp docs example.
+# The url will join {http_path} variable.
+module ViewHelpers
+  def new_link_to(name, href, options={})
+    href = File.join(Helpers::Path.get_http_path, href)
+    href = ensure_path(href, '')
+    if href == request.path
+      options[:class] ||=''
+      options[:class] += "active"
+    end
+    link_to name, href, options
+  end
+end
+
+# @authors Casper Lai (casper.lai@sleepingdesign.com)
+# @date    2014-01-07 15:02:32
+# @version 1.0.0.0
+
+module Helpers
+  # the module get compass config {http_path} varitable.
+  module Path
+    def self.get_http_path
+      if defined? Compass
+          if defined? Compass.configuration.http_path
+                Compass.configuration.http_path
+          else
+            ''
+          end
+        end
+    end
+  end
+end
